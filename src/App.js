0c19278704576d328f02e8243cc12d0e95f75088
Ashley Kubizna <113566558+kubizna21@users.noreply.github.com>
2023-04-07 15:28:15 -0700

    adding things

---
+++ src/App.js
@@ -0,0 +1,35 @@
+import React, { useState } from 'react';
+import ColorBlock from './components/ColorBlock';
+import ColorForm from './components/ColorForm';
+import './App.css';
+
+const DEFAULT_COLOR = [
+  'red',
+  'yellow',
+  'magenta',
+  'black',
+  'gray',
+  'teal'
+]
+
+
+function App() {
+  let [colors, setColors] = useState(DEFAULT_COLOR);
+
+  const addToColors = newColor => {
+    setColors([...colors, newColor])
+  }
+
+
+  let colorMap = colors.map((colorString, index) => {
+    return <ColorBlock color={colorString} key={index} />
+  });
+  return (
+    <div className="App">
+      {colorMap}
+      <ColorForm addToColors={addToColors} />
+    </div>
+  );
+}
+
+export default App;
